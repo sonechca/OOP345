b7cb2e9d72c7af7ebe899ea89500f52876b42344
mintae.kim <mkim221@myseneca.ca>
2019-09-30 23:35:19 -0400

    modify header files and update reflect

--- Workshop3_home/LVPair.h
+++ Workshop3_home/LVPair.h
@@ -1,7 +1,7 @@
 // Name: Mintae Kim
 // Seneca Student ID: 141915181
 // Seneca email: mkim221@myseneca.ca
-// Date of completion: Sep 26, 2019
+// Date of completion: Sep 30, 2019
 //
 // I confirm that the content of this file is created by me,
 //   with the exception of the parts provided to me by my professor.
@@ -21,18 +21,19 @@ namespace sdds {
         LVPair(): typeName{}, typeValue{} {} // Constuctor
         LVPair(const L& aa, const V& bb): typeName(aa),typeValue(bb){}; //constuctor with parameters
         const L& key() const{
-            return &typeName;
+            return typeName;
         }
         const V& value() const{
-            return &typeValue;
+            return typeValue;
         }
         virtual void display(std::ostream& os) const{
             os << key() << " : " << value() << std::endl; //output
         }
     };
+
 // helper function for helping display
 template <typename L, typename V>
-std::ostream& operator<<(std::ostream& os, const sdds::LVPair<L, V>& pair){
+std::ostream& operator<<(std::ostream& os, const LVPair<L, V>& pair){
     pair.display(os);
     return os;
 }
@@ -46,35 +47,43 @@ public:
     static const V& getInitialValue() {
         return initialValue;
     }
-    
-    SummableLVPair(): LVPair<L, V>() {
-        initialValue = {};
-        minimumField = 0;
-    }
+    SummableLVPair(){}
     SummableLVPair(const L& lbl, const V& val) : LVPair<L, V>(lbl, val) {
-        
+        if(minimumField < lbl.size()){
+            minimumField = lbl.size();
+        }
     }
     
     V sum(const L& lbl, const V& val) const {
-        return LVPair<L, V>::value() + val;
+        if(LVPair<L, V>::key() == lbl)
+            return LVPair<L, V>::value() + val;
+        else
+            return val;
     }
     
     void display(std::ostream& os) const {
-        os << LVPair<L, V>::key() << ": " << LVPair<L, V>::value() << std::endl;
+        os.setf(std::ios::left);
+        os.width(minimumField);
+        LVPair<L, V>::display(os);
+        os.unsetf(std::ios::left);
     }
    
 };
+template <typename L, typename V>
+size_t SummableLVPair<L, V>::minimumField = 0; //initialize for static value
+
 template<>
-SummableLVPair<std::string, std::string>::SummableLVPair(){
-       
-   }
+std::string SummableLVPair<std::string, std::string>::initialValue = ""; // initialize string type with template specialize
    
 template<>
-SummableLVPair<std::string, int>::SummableLVPair(){}
+int SummableLVPair<std::string, int>::initialValue = 0; // initialize int type with template specialize
    
 template<>
-std::string SummableLVPair<std::string, std::string>::sum(const std::string& lbl, const std::string& val) const{
-    return val + ", " + LVPair<std::string, std::string>::value();
+std::string SummableLVPair<std::string, std::string>::sum(const std::string& lbl, const std::string& val) const{ //specializing the `sum()` member function.
+    if(val == "")
+        return LVPair<std::string, std::string>::value();
+    else
+        return val + ", " + LVPair<std::string, std::string>::value();
 }
 }
 
--- Workshop3_home/List.h
+++ Workshop3_home/List.h
@@ -1,7 +1,7 @@
 // Name: Mintae Kim
 // Seneca Student ID: 141915181
 // Seneca email: mkim221@myseneca.ca
-// Date of completion: Sep 26, 2019
+// Date of completion: Sep 30, 2019
 //
 // I confirm that the content of this file is created by me,
 //   with the exception of the parts provided to me by my professor.
@@ -14,8 +14,9 @@ namespace sdds {
     template <typename T, int N> // Declare template with a non-type template parameter>
     class List {
         T a[N];
-        size_t addListElement = 0; // variable for adding list elements
+        size_t addListElement; // variable for adding list elements
     public:
+        List():addListElement{ 0u }{}
         size_t size() const{
             return addListElement;
         }
@@ -32,11 +33,13 @@ namespace sdds {
     template <typename L, typename V, typename T, int N>
     class LVList : public List<T, N> {
     public:
-        V accumulate(const L& label) const {
-            SummableLVPair<L, V> initValue;
-            V accumulValue = initValue.getInitialValue();
-            for(size_t i = 0; i < N; i++){
-                accumulValue = initValue.sum(label, accumulValue);
+        V accumulate(const L& label) const { // accumulate all the values
+            V accumulValue = SummableLVPair<L, V>::getInitialValue();
+    
+            for(size_t i = 0; i < ((List<T, N>&) * this).size(); i++){
+                if(label == ((List<T, N>&) * this)[i].key()){
+                    accumulValue = (*this)[i].sum(label, accumulValue);
+                }
             }
             return accumulValue;
         }
